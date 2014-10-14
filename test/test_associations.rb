897dcdaf66f6a52d2a1f92c0a96efcdb8e0aa790
Scott Barrow <scott@guestfolio.com>
2014-10-14 13:17:29 -0700

    spec

--- test/test_associations.rb
+++ test/test_associations.rb
@@ -32,6 +32,24 @@ class AssociationsTest < ParanoidBaseTest
     assert_equal 0, ParanoidProduct.with_deleted.count
   end
 
+  def test_joins_has_many_association_should_exclude_deleted_record
+    paranoid_one = ParanoidTime.create! :name => 'bla bla one!'
+    paranoid_one_many = paranoid_one.paranoid_has_many_dependants.create! :name => 'bla bla one many!'
+    paranoid_one_with_deleted = ParanoidTime.create! :name => 'bla bla another!'
+    paranoid_one_many_deleted = paranoid_one_with_deleted.paranoid_has_many_dependants.create! :name => 'bla bla one many!', :deleted_at => Time.now
+    binding.pry
+    assert ParanoidTime.joins(:paranoid_has_many_dependants) == [paranoid_one], "has_many should only contain the #{paranoid_one.inspect} record"
+  end
+
+  def test_joins_belongs_to_association_should_exclude_deleted_record
+    paranoid_one = ParanoidTime.create! :name => 'bla bla one!'
+    paranoid_one_many = paranoid_one.paranoid_has_many_dependants.create! :name => 'bla bla one many!'
+    paranoid_one_with_deleted = ParanoidTime.create! :name => 'bla bla another!', :deleted_at => Time.now
+    paranoid_one_many_deleted = paranoid_one_with_deleted.paranoid_has_many_dependants.create! :name=> 'bla bla one many with deleted!'
+
+    assert ParanoidHasManyDependant.joins(:paranoid_time) == [paranoid_one_many], "belongs_to should only contain the #{paranoid_one_many.inspect} record"
+  end
+
   def test_belongs_to_with_deleted
     paranoid_time = ParanoidTime.first
     paranoid_has_many_dependant = paranoid_time.paranoid_has_many_dependants.create(:name => 'dependant!')
