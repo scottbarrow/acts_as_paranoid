192378d03fe745ee1cc1c3ff58ea4a77de2e54bf
Scott Barrow <scott@guestfolio.com>
2014-10-15 10:47:22 -0700

    add alias method chain

--- lib/acts_as_paranoid/join_association.rb
+++ lib/acts_as_paranoid/join_association.rb
@@ -2,22 +2,21 @@ module ActsAsParanoid
   module JoinAssociation
     def self.included(base)
       base.class_eval do
-
-        alias_method :orig_build_constraint, :build_constraint
-        # Add paranoid_default_scope_sql as conditions when join other table
-        # <t>Blog.joins(:users)</t> would produce
-        # <t>select blogs.* from blogs inner join users on users.id = blogs.user_id AND users.deleted_at is NULL</t>
-        # ref https://github.com/goncalossilva/acts_as_paranoid/issues/71
-        def build_constraint(reflection, table, key, foreign_table, foreign_key)
-          constraint = orig_build_constraint(reflection, table, key, foreign_table, foreign_key)
-
-          if reflection.klass.paranoid?
-            constraint = constraint.and(table[reflection.klass.paranoid_column].eq(nil))
-          end
-          constraint
-        end
-
+        alias_method_chain :build_constraint, :deleted
       end
+    end
+
+    # Add paranoid_default_scope_sql as conditions when join other table
+    # <t>Blog.joins(:users)</t> would produce
+    # <t>select blogs.* from blogs inner join users on users.id = blogs.user_id AND users.deleted_at is NULL</t>
+    # ref https://github.com/goncalossilva/acts_as_paranoid/issues/71
+    def build_constraint_with_deleted(reflection, table, key, foreign_table, foreign_key)
+      constraint = build_constraint_without_deleted(reflection, table, key, foreign_table, foreign_key)
+
+      if reflection.klass.paranoid?
+        constraint = constraint.and(table[reflection.klass.paranoid_column].eq(nil))
+      end
+      constraint
     end
   end
 end
